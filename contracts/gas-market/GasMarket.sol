b81d3447d27d037d57b6ecf0c30ccee7ad45acca
Benjamin Bollen <ben@ost.com>
2019-07-05 20:08:50 +0400

    gas market: stub

--- contracts/gas-market/GasMarket.sol
+++ contracts/gas-market/GasMarket.sol
@@ -15,7 +15,106 @@ pragma solidity ^0.5.0;
 // limitations under the License.
 
 import "../utility-token/contracts/UtilityToken.sol";
+import "../utility-token/contracts/organization/contracts/OrganizationInterface.sol";
 
 contract GasMarket is UtilityToken {
 
+    /* Storage */
+
+    /** Address of OST on mainnet */
+    address public constant OST_TOKEN_MAINNET = address(0x2C4e8f2D746113d0696cE89B35F0d8bF88E0AEcA);
+
+    /** Symbol of OST, note the symbol is "ST" on mainnet contract */
+    string public constant OST_TOKEN_SYMBOL = "OST";
+
+    /** Name of OST */
+    string public constant OST_TOKEN_NAME = "Simple Token";
+
+    /** Decimals of OST */
+    uint8 public constant OST_TOKEN_DECIMALS = 18;
+
+    /** Kernel gateway contract address */
+    address public kernelGateway;
+
+    modifier onlyKernelGateway()
+    {
+        require(msg.sender == kernelGateway,
+            "Only gateway can call this function.");
+        _;
+    }
+
+    /* External / public functions */
+
+    constructor(
+        address _kernelGateway,
+        OrganizationInterface _organization
+    )
+        public
+        UtilityToken(
+            OST_TOKEN_MAINNET,
+            OST_TOKEN_SYMBOL,
+            OST_TOKEN_NAME,
+            OST_TOKEN_DECIMALS,
+            _organization
+        )
+    {
+        require(_kernelGateway != address(0),
+            "Kernel gateway must not be null.");
+
+        kernelGateway = _kernelGateway;
+    }
+
+    // function registerKernel(
+    //     uint256 _height,
+
+    // )
+    //     external
+    //     onlyKernelGateway
+    // {
+
+    // }
+
+    function increaseSupply(
+        address payable _account,
+        uint256 _amount
+    )
+        external
+        onlyKernelGateway
+        returns (bool success_)
+    {
+        // Supply can only be increased to deposit in the gas market.
+        // note: don't require this condition to allow the gateway to
+        // record the failure of the message.
+        if (_account == address(this)) {
+            // deposit OST into the gas market
+            success_ = super.increaseSupplyInternal(address(this), _amount);
+        } else {
+            success_ = false;
+        }
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success_)
+    {
+        require(false,
+            "Transfers on the gas market are not allowed.");
+        success_ = false;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success_)
+    {
+        require(false,
+            "Transfers on the gas market are not allowed.");
+        success_ = false;
+    }
 }
