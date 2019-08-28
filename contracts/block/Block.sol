24c17723a4fc740bd8f4380ae6ba8d56589493b5
Paruyr Gevorgyan <paruyr@ost.com>
2019-08-28 16:44:44 +0200

    Adjusted implementation of anchor implementation from Mosaic-0

--- contracts/block/Block.sol
+++ contracts/block/Block.sol
@@ -14,7 +14,7 @@ pragma solidity >=0.5.0 <0.6.0;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import "../rlp/RLP.sol";
+import "../lib/RLP.sol";
 
 /**
  * @title Block provides functionality to work with blocks.
