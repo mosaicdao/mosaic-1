b32806c94dee86483a79e266ab380f463ea854a8
abhayks1 <abhay@ost.com>
2019-11-21 15:19:28 +0530

    form committee travis fix

--- test/consensus/form_committee.js
+++ test/consensus/form_committee.js
@@ -54,6 +54,30 @@ contract('Consensus::formCommittee', (accounts) => {
   });
 
   contract('Negative Tests', async () => {
+    beforeEach(async () => {
+      consensus = await Consensus.new();
+      axiom = await SpyAxiom.new();
+
+      await axiom.setupConsensus(consensus.address);
+
+      // Populate the input data.
+      testInputs.committeeSize = new BN(100);
+      testInputs.coreAddress = accountProvider.get();
+      testInputs.proposal = Utils.getRandomHash();
+
+      await consensus.setCoreStatus(
+        testInputs.coreAddress,
+        CoreStatusUtils.CoreStatus.creation,
+      );
+
+      await consensus.registerPrecommit(
+        testInputs.proposal,
+        {
+          from: testInputs.coreAddress,
+        },
+      );
+    });
+
     it('should fail when pre-commit proposal does not exists for a given core address', async () => {
       const coreAddress = accountProvider.get();
 
@@ -145,6 +169,27 @@ contract('Consensus::formCommittee', (accounts) => {
   contract('Positive Tests', () => {
     let committeeFormationBlockHeight;
     beforeEach(async () => {
+      consensus = await Consensus.new();
+      axiom = await SpyAxiom.new();
+
+      await axiom.setupConsensus(consensus.address);
+
+      // Populate the input data.
+      testInputs.committeeSize = new BN(100);
+      testInputs.coreAddress = accountProvider.get();
+      testInputs.proposal = Utils.getRandomHash();
+
+      await consensus.setCoreStatus(
+        testInputs.coreAddress,
+        CoreStatusUtils.CoreStatus.creation,
+      );
+
+      await consensus.registerPrecommit(
+        testInputs.proposal,
+        {
+          from: testInputs.coreAddress,
+        },
+      );
       const initialBlockNumber = await Utils.getBlockNumber();
       committeeFormationBlockHeight = initialBlockNumber
         .addn(consensusUtil.CommitteeFormationDelay);
