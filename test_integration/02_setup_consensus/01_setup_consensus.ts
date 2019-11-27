7850d20ff9bd32b160950d2c4d3d9209304d7512
abhayks1 <abhay@ost.com>
2019-11-27 13:30:18 +0530

    setupConsensus in progress

---
+++ test_integration/02_setup_consensus/01_setup_consensus.ts
@@ -0,0 +1,99 @@
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import shared from '../shared';
+import Interacts from "../../interacts/Interacts";
+
+describe('Axiom::setupConsensus', async () => {
+  it('TechGov calls Axiom.setupConsensus', async () => {
+
+    const axiomInstance = shared.origin.contracts.Axiom;
+    const committeeSize = '3';
+    const minValidators = '5';
+    const joinLimit = '5';
+    const gasTargetDelta = '';
+    const coinbaseSplitPerMille = '';
+    const mOSTAddress = '';
+    const stakeMOSTAmount = '';
+    const wETHAddress = '';
+    const stakeWETHAmount = '';
+    const cashableEarningsPerMille = '';
+    const initialReputation = '1';
+    const withdrawalCooldownPeriodInBlocks = '';
+    const txOptions = {
+      from: shared.origin.keys.techGov,
+    };
+    await axiomInstance.methods.setupConsensus(
+      committeeSize,
+      minValidators,
+      joinLimit,
+      gasTargetDelta,
+      coinbaseSplitPerMille,
+      mOSTAddress,
+      stakeMOSTAmount,
+      wETHAddress,
+      stakeWETHAmount,
+      cashableEarningsPerMille,
+      initialReputation,
+      withdrawalCooldownPeriodInBlocks,
+    ).send(
+      // TODO
+    );
+    const reputationContractAddress = await axiomInstance.methods.reputation().call();
+    shared.origin.contracts.Reputation = Interacts.getReputation(
+      shared.origin.web3,
+      reputationContractAddress,
+    );
+
+    const consensusContractAddress = await axiomInstance.methods.consensus().call();
+    shared.origin.contracts.Consensus = Interacts.getConsensus(
+      shared.origin.web3,
+      consensusContractAddress,
+    );
+
+  });
+
+  it('TechGov calls Axiom.newMetaChain', async () => {
+
+    // const axiomInstance = shared.origin.contracts.Axiom;
+    // const committeeSize = '3';
+    // const minValidators = '5';
+    // const joinLimit = '5';
+    // const gasTargetDelta = '';
+    // const coinbaseSplitPerMille = '';
+    // const mOSTAddress = '';
+    // const stakeMOSTAmount = '';
+    // const wETHAddress = '';
+    // const stakeWETHAmount = '';
+    // const cashableEarningsPerMille = '';
+    // const initialReputation = '1';
+    // const withdrawalCooldownPeriodInBlocks = '';
+    // await axiomInstance.methods.setupConsensus(
+    //   committeeSize,
+    //   minValidators,
+    //   joinLimit,
+    //   gasTargetDelta,
+    //   coinbaseSplitPerMille,
+    //   mOSTAddress,
+    //   stakeMOSTAmount,
+    //   wETHAddress,
+    //   stakeWETHAmount,
+    //   cashableEarningsPerMille,
+    //   initialReputation,
+    //   withdrawalCooldownPeriodInBlocks,
+    // );
+
+  });
+
+});
---
+++ test_integration/03_new_metachain/01_new_metachain.ts
@@ -0,0 +1,52 @@
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import shared from '../shared';
+import Interacts from "../../interacts/Interacts";
+
+describe('Axiom::newMetaChain', async () => {
+
+  it('TechGov calls Axiom.newMetaChain', async () => {
+
+    // const axiomInstance = shared.origin.contracts.Axiom;
+    // const committeeSize = '3';
+    // const minValidators = '5';
+    // const joinLimit = '5';
+    // const gasTargetDelta = '';
+    // const coinbaseSplitPerMille = '';
+    // const mOSTAddress = '';
+    // const stakeMOSTAmount = '';
+    // const wETHAddress = '';
+    // const stakeWETHAmount = '';
+    // const cashableEarningsPerMille = '';
+    // const initialReputation = '1';
+    // const withdrawalCooldownPeriodInBlocks = '';
+    // await axiomInstance.methods.setupConsensus(
+    //   committeeSize,
+    //   minValidators,
+    //   joinLimit,
+    //   gasTargetDelta,
+    //   coinbaseSplitPerMille,
+    //   mOSTAddress,
+    //   stakeMOSTAmount,
+    //   wETHAddress,
+    //   stakeWETHAmount,
+    //   cashableEarningsPerMille,
+    //   initialReputation,
+    //   withdrawalCooldownPeriodInBlocks,
+    // );
+
+  });
+
+});
