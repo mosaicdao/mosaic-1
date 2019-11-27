9d1dbe612b31b2713b6d54c2fdb6af50b61a948d
Paruyr Gevorgyan <paruyr@ost.com>
2019-11-27 13:55:36 +0100

    Fixes include path for Core.sol

--- contracts/test/core/MockCore.sol
+++ contracts/test/core/MockCore.sol
@@ -14,7 +14,7 @@ pragma solidity ^0.5.0;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import "../../Core/Core.sol";
+import "../../core/Core.sol";
 
 contract MockCore is Core {
 
