182df93454755cf1eaebf7ea4351ec7dd970f0d4
sarvesh <sarvesh@ost.com>
2019-11-27 14:55:58 +0530

    Added sendTransaction method.

--- test_integration/Utils.ts
+++ test_integration/Utils.ts
@@ -1,6 +1,14 @@
+import {TransactionObject} from "../interacts/types";
 
 
 export default class Utils {
+  /**
+   * Fund address for gas with ETH
+   * @param beneficiary Beneficiary Address.
+   * @param funder Funder Address.
+   * @param web3 Web3 instance.
+   * @param value Amount in wei.
+   */
   static fundAddressForGas(beneficiary: string, funder:string, web3, value: string) {
     return web3.eth.sendTransaction(
       {
@@ -9,6 +17,24 @@ export default class Utils {
         value,
       },
     );
+  }
+
+  /**
+   * Send Transaction.
+   * @param rawTx Raw Transaction object.
+   * @param txOptions Transaction Options.
+   */
+  static async sendTransaction(
+    rawTx: any,
+    txOptions: {
+      gas?: string,
+      from: string
+    }) {
+    txOptions.gas = txOptions.gas
+      ? txOptions.gas
+      : (await rawTx.estimateGas()).toString();
+
+    return rawTx.send(txOptions);
   }
 }
 
