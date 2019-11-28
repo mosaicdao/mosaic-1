0e8e28622c23ff16b2875038949f8a1d01c4ec2f
Gulshan Vasnani <gulshan@ost.com>
2019-11-28 13:06:05 +0530

    Added new metachain tests

--- test_integration/03_new_metachain/01_new_metachain.ts
+++ test_integration/03_new_metachain/01_new_metachain.ts
@@ -14,23 +14,25 @@
 
 import shared from '../shared';
 import Interacts from "../../interacts/Interacts";
+import Utils from "../Utils";
 
 describe('Axiom::newMetaChain', async () => {
 
-  // it('TechGov calls Axiom.newMetaChain', async () => {
-  //   const axiomInstance = shared.origin.contracts.Axiom;
-  //   const maxStateRoots = '';
-  //   const rootRlpBlockHeader = '';
-  //   const txOptions = {
-  //     from: shared.origin.keys.techGov,
-  //   };
-  //   await axiomInstance.methods.newMetaChain(
-  //     maxStateRoots,
-  //     rootRlpBlockHeader,
-  //   );
-  //   // TODO send transaction
-  //   // TODO set chainID in shared
-  //
-  // });
+  it('Proposes metablock', async () => {
+
+    const axiom = shared.origin.contracts.Axiom;
+    const stateRoots = 100;
+    const rlpBlockHeader = '0xf901f9a083cafc574e1f51ba9dc0568fc617a08ea2429fb384059c972f13b19fa1c8dd55a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347948888f1f195afa192cfee860698584c030f4c9db1a0ef1552a40b7165c3cd773806b9e0c165b75356e0314bf0706f279c729f51e017a05fe50b260da6308036625b850b5d6ced6d0a9f814c0688bc91ffb7b7a3a54b67a0bc37d79753ad738a6dac4921e57392f145d8887476de3f783dfa7edae9283e52b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008302000001832fefd8825208845506eb0780a0bd4472abb6659ebe3ee06ee4d7b72a00a9f4d001caca51342001075469aff49888a13a5a8c8f2bb1c4';
+    const rawTx = axiom.instance.methods.newMetaChain(
+      stateRoots,
+      rlpBlockHeader as any,
+    );
+    console.log('consensus address  in axiom :- ',await axiom.instance.methods.consensus().call());
+    await Utils.sendTransaction(rawTx, {
+      from: shared.origin.keys.techGov,
+    });
+    const consensusContractInstance = Interacts.getConsensus(await axiom.instance.methods.consensus().call());
+
+  });
 
 });
