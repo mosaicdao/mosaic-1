4d626e029716a5dd7d4994e9840ebedba8c1b5a2
Paruyr Gevorgyan <paruyr@ost.com>
2019-12-06 12:42:33 +0100

    Remove ganache --verbose & --debug flags

--- tools/run_ganache_cli.sh
+++ tools/run_ganache_cli.sh
@@ -5,6 +5,4 @@ script_dir_path="$(cd "$(dirname "$0")" && pwd)"
 "${script_dir_path}/../node_modules/.bin/ganache-cli" \
     --accounts=1000 \
     --defaultBalanceEther=10000 \
-    --gasLimit 0xfffffffffff \
-    --verbose \
-    --debug
+    --gasLimit 0xfffffffffff
