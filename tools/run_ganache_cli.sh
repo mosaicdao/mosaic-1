f4a4bc4f4add80d9351a108eebf3cdd08f0d0e77
Paruyr Gevorgyan <paruyr@ost.com>
2019-12-06 11:02:02 +0100

    Enabling --noVMErrorsOnRPCResponse mode of ganache-cli

--- tools/run_ganache_cli.sh
+++ tools/run_ganache_cli.sh
@@ -5,4 +5,5 @@ script_dir_path="$(cd "$(dirname "$0")" && pwd)"
 "${script_dir_path}/../node_modules/.bin/ganache-cli" \
     --accounts=1000 \
     --defaultBalanceEther=10000 \
-    --gasLimit 0xfffffffffff
+    --gasLimit 0xfffffffffff \
+    --noVMErrorsOnRPCResponse
