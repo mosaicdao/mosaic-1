c178fc958fa195981e896935b91c95279aac183a
Deepesh K N <deepesh@ost.com>
2019-12-06 14:37:01 +0530

    Message box cleanup

---
+++ contracts/message-bus/MessageBox.sol
@@ -0,0 +1,83 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import "../version/MosaicVersion.sol";
+
+contract MessageBox is MosaicVersion {
+
+    /* Constants */
+
+    /** EIP-712 domain separator name for Core */
+    string public constant DOMAIN_SEPARATOR_NAME = "Message-Bus";
+
+    // TODO: change `bytes20 chainId` to `bytes32 metachainId`.
+    /** EIP-712 domain separator for Core */
+    bytes32 public constant DOMAIN_SEPARATOR_TYPEHASH = keccak256(
+        "EIP712Domain(string name,string version,bytes20 chainId,address verifyingContract)"
+    );
+
+    /** Message type hash */
+    bytes32 public constant MESSAGE_TYPEHASH = keccak256(
+        abi.encode(
+            "Message(bytes32 intentHash,uint256 nonce,uint256 gasPrice,uint256 gasLimit,address sender)"
+        )
+    );
+
+
+    /* Internal functions */
+
+    /**
+     * @notice Generate message hash from the input params
+     * @param _intentHash Intent hash of message.
+     * @param _nonce Nonce of sender.
+     * @param _gasPrice Gas price.
+     * @param _gasLimit Gas limit.
+     * @param _sender Sender address.
+     * @return messageHash_ Message hash.
+     */
+    function _messageHash(
+        bytes32 _intentHash,
+        uint256 _nonce,
+        uint256 _gasPrice,
+        uint256 _gasLimit,
+        address _sender,
+        bytes32 _domainSeparator
+    )
+        internal
+        pure
+        returns (bytes32 messageHash_)
+    {
+        bytes32 typedMessageHash = keccak256(
+            abi.encode(
+                MESSAGE_TYPEHASH,
+                _intentHash,
+                _nonce,
+                _gasPrice,
+                _gasLimit,
+                _sender
+            )
+        );
+
+        messageHash_ = keccak256(
+            abi.encodePacked(
+                byte(0x19),
+                byte(0x01),
+                _domainSeparator,
+                typedMessageHash
+            )
+        );
+    }
+}
---
+++ contracts/message-bus/MessageInbox.sol
@@ -0,0 +1,216 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import "./MessageBox.sol";
+import "./StateRootI.sol";
+import "./Proof.sol";
+
+contract MessageInbox is MessageBox, Proof {
+
+    // TODO: If we want to revert, may be the value will change to MessageBoxEnum.(still to think on this)
+    /** Mapping to indicate that message hash exists in inbox. */
+    mapping(bytes32 => bool) public inbox;
+
+    /** Domain separator for inbox */
+    bytes32 public inboxDomainSeparator;
+
+    /** Message outbox address */
+    address public messageOutbox;
+
+    /** Outbox storage index */
+    uint8 outboxStorageIndex;
+
+
+    /* External Functions. */
+
+    /**
+     * @notice Generate inbox message hash from the input params
+     * @param _intentHash Intent hash of message.
+     * @param _nonce Nonce of sender.
+     * @param _gasPrice Gas price.
+     * @param _gasLimit Gas limit.
+     * @param _sender Sender address.
+     * @return messageHash_ Message hash.
+     */
+    function inboxMessageHash(
+        bytes32 _intentHash,
+        uint256 _nonce,
+        uint256 _gasPrice,
+        uint256 _gasLimit,
+        address _sender
+    )
+        external
+        view
+        returns (bytes32 messageHash_)
+    {
+        messageHash_ = _messageHash(
+            _intentHash,
+            _nonce,
+            _gasPrice,
+            _gasLimit,
+            _sender,
+            inboxDomainSeparator
+        );
+    }
+
+
+    /* Internal Functions. */
+
+    // TODO: change `chainId` to `metachainId`
+    /**
+     * @notice Setup message inbox.
+     * @param _chainId Chain identifier.
+     * @param _messageOutbox MessageOutbox contract address.
+     * @param _outboxStorageIndex Storage index of outbox mapping in
+     *                            MessageOutbox contract.
+     * @param _stateRootProvider State root provider contract address.
+     * @param _maxStorageRootItems Defines how many storage roots should be
+     *                             stored in circular buffer.
+     * @param _verifyingAddress Address of verifying contract.
+     */
+    function setupMessageInbox(
+        bytes20 _chainId,
+        address _messageOutbox,
+        uint8 _outboxStorageIndex,
+        StateRootI _stateRootProvider,
+        uint256 _maxStorageRootItems,
+        address _verifyingAddress
+    )
+        internal
+    {
+        require(
+            inboxDomainSeparator == bytes32(0),
+            "Message inbox is already setup."
+        );
+
+        require(
+            _chainId != bytes20(0),
+            "Chain id is 0."
+        );
+
+        require(
+            _messageOutbox != address(0),
+            "Inbox address is 0."
+        );
+
+        require(
+            _verifyingAddress != address(0),
+            "Verifying address is 0."
+        );
+
+        messageOutbox = _messageOutbox;
+        outboxStorageIndex = _outboxStorageIndex;
+
+        inboxDomainSeparator = keccak256(
+            abi.encode(
+                DOMAIN_SEPARATOR_TYPEHASH,
+                DOMAIN_SEPARATOR_NAME,
+                DOMAIN_SEPARATOR_VERSION,
+                _chainId,
+                _verifyingAddress
+            )
+        );
+
+        /* @dev: Please note that no validations are done here. Proof::setup
+         *       already has the validations for the input params, so avoided
+         *       the duplications here.
+         */
+        Proof.initialize(
+            _messageOutbox,
+            _stateRootProvider,
+            _maxStorageRootItems
+        );
+    }
+
+    /**
+     * @notice Verify merkle proof of a storage contract address.
+     *         Trust factor is brought by state roots of the contract which
+     *         implements StateRootInterface.
+     * @param _blockHeight Block height at which Gateway/CoGateway is to be
+     *                     proven.
+     * @param _rlpAccount RLP encoded account node object.
+     * @param _rlpParentNodes RLP encoded value of account proof parent nodes.
+     */
+    function proveStorageAccount(
+        uint256 _blockHeight,
+        bytes memory _rlpAccount,
+        bytes memory _rlpParentNodes
+    )
+        internal
+    {
+        Proof.proveStorageAccount(
+            _blockHeight,
+            _rlpAccount,
+            _rlpParentNodes
+        );
+    }
+
+    /**
+     * @notice Confirm a new message that is declared in outbox on the source
+     *         chain. Merkle proof will be performed to verify storage data.
+     *         This will update the inbox value to `true` for the given message hash.
+     * @param _intentHash Intent hash of message.
+     * @param _nonce Nonce of sender.
+     * @param _gasPrice Gas price.
+     * @param _gasLimit Gas limit.
+     * @param _sender Sender address.
+     * @param _blockHeight Block height for fetching storage root.
+     * @param _rlpParentNodes RLP encoded parent node data to prove in
+     *                        messageBox outbox.
+     * @return messageHash_ Message hash.
+     */
+    function confirmMessage(
+        bytes32 _intentHash,
+        uint256 _nonce,
+        uint256 _gasPrice,
+        uint256 _gasLimit,
+        address _sender,
+        uint256 _blockHeight,
+        bytes memory _rlpParentNodes
+    )
+        internal
+        returns (bytes32 messageHash_)
+    {
+        messageHash_ = _messageHash(
+            _intentHash,
+            _nonce,
+            _gasPrice,
+            _gasLimit,
+            _sender,
+            inboxDomainSeparator
+        );
+
+        require(
+            inbox[messageHash_] == false,
+            "Message already exists in the inbox."
+        );
+
+        inbox[messageHash_] = true;
+
+        // Get the storage path to verify proof.
+        bytes memory path = Proof.storagePath(
+            outboxStorageIndex,
+            messageHash_
+        );
+
+        Proof.proveStorageExistence(
+            path,
+            keccak256(abi.encodePacked(true)),
+            _blockHeight,
+            _rlpParentNodes
+        );
+    }
+}
