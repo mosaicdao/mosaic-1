8402cf43ca24408d0e14635ac781d4aa4bf592e1
Gulshan Vasnani <gulshanvasnani@Gulshans-MacBook-Pro.local>
2020-01-09 15:36:33 +0530

    Fixed travis

--- contracts/test/consensus_cogateway/TestConsensusCogateway.sol
+++ contracts/test/consensus_cogateway/TestConsensusCogateway.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0 <0.6.0;
-import "../../consensus-gateway/ConsensusCoGateway.sol";
+import "../../consensus-gateway/ConsensusCogateway.sol";
 
 /**
  * @title TestConsensusCogateway
