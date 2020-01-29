d7bb288559fafab35316e3673e9c69b1cd4be5ad
abhayks1 <abhay@ost.com>
2020-01-29 13:41:16 +0530

    Added functions in Coconsensus interface

--- contracts/consensus/CoconsensusI.sol
+++ contracts/consensus/CoconsensusI.sol
@@ -20,13 +20,70 @@ pragma solidity >=0.5.0 <0.6.0;
  * @notice Interface for Coconsensus contract.
  */
 interface CoconsensusI {
+    /** External Functions */
 
     /**
-     * @notice Gets the anchor address for an metachain id.
+     * @notice Gets the anchor address for a metachain id.
+     *
+     * @param _metachainId Metachain id.
      *
      * @return Anchor contract address.
      */
-    function getAnchor(bytes32 _metachainId) external returns(address);
+    function getAnchor(bytes32 _metachainId) external returns (address);
+
+    /**
+     * @notice Finalizes the checkpoint.
+     *
+     * @param _metachainId Metachain id.
+     * @param _number Block number.
+     * @param _blockHash Block hash.
+     */
+    function finalizeCheckPoint(
+        bytes32 _metachainId,
+        uint256 _number,
+        bytes32 _blockHash
+    )
+        external;
+
+    /**
+     * @notice Commit the checkpoint.
+     *
+     * @param _metachainId Metachain id.
+     * @param _kernelHeight Kernel height.
+     * @param _updatedValidators Array of updated validators.
+     * @param _updatedReputation Array of updated reputation.
+     * @param _gasTarget Gas target.
+     * @param _transitionHash Transition hash.
+     * @param _source Source block hash.
+     * @param _target Target block hash.
+     * @param _sourceBlockNumber Source block number.
+     * @param _targetBlockNumber Target block number.
+     */
+    function commitCheckpoint(
+        bytes32 _metachainId,
+        uint256 _kernelHeight,
+        address[] calldata _updatedValidators,
+        uint256[] calldata _updatedReputation,
+        uint256 _gasTarget,
+        bytes32 _transitionHash,
+        bytes32 _source,
+        bytes32 _target,
+        uint256 _sourceBlockNumber,
+        uint256 _targetBlockNumber
+    )
+        external;
+
+    /**
+     * @notice Decodes the RLP encoded bytes to extract the block number
+     *         and state root. Calls anchorStateRoot to store state root.
+     *
+     * @param _metachainId Metachain id.
+     * @param _rlpBlockHeader RLP encoded block header.
+     */
+    function observeBlock(
+        bytes32 _metachainId,
+        bytes calldata _rlpBlockHeader
+    )
+        external;
 
 }
-
