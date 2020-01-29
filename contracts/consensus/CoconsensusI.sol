c0edbf40c483badeadc5c3109918a0140349cab2
abhayks1 <abhay@ost.com>
2020-01-29 18:37:09 +0530

    formatting update

--- contracts/consensus/CoconsensusI.sol
+++ contracts/consensus/CoconsensusI.sol
@@ -21,7 +21,7 @@ pragma solidity >=0.5.0 <0.6.0;
  */
 interface CoconsensusI {
 
-    /** External Functions */
+    /* External Functions */
 
     /**
      * @notice Gets the anchor address for a metachain id.
