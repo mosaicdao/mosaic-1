928579bb19373ead122acd585b92e8fb3ccafe64
Gulshan Vasnani <gulshan@ost.com>
2020-01-29 19:49:30 +0530

    resolve merge conflicts

--- contracts/protocore/SelfProtocore.sol
+++ contracts/protocore/SelfProtocore.sol
@@ -19,11 +19,24 @@ import "../protocore/GenesisSelfProtocore.sol";
 import "../proxies/MasterCopyNonUpgradable.sol";
 import "../validator/ValidatorSet.sol";
 
-
 /**
  * @title Self protocore - This contract finalizes the proposed blocks of auxiliary chain.
  */
 contract SelfProtocore is MasterCopyNonUpgradable, GenesisSelfProtocore, ValidatorSet, Protocore {
+
+    /* Events */
+
+    event LinkProposed(
+        bytes32 _parentVoteMessageHash,
+        bytes32 _targetBlockHash,
+        uint256 _targetBlockNumber,
+        bytes32 _sourceOriginObservation,
+        bytes32 _sourceKernelHash,
+        uint256 _sourceDynasty,
+        uint256 _sourceAccumulatedGas,
+        bytes32 _sourceCommitteeLock
+    );
+
 
     /* Special Functions */
 
@@ -107,5 +120,4 @@ contract SelfProtocore is MasterCopyNonUpgradable, GenesisSelfProtocore, Validat
             }
         }
     }
-
 }
