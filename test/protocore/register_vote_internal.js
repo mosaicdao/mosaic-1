542bfb28d749a41ce1b6169bff8de6af76601acd
Paruyr Gevorgyan <paruyr@ost.com>
2020-01-30 16:32:36 +0100

    Adds basic unit test cases schema

--- test/protocore/register_vote_internal.js
+++ test/protocore/register_vote_internal.js
@@ -176,4 +176,14 @@ contract('Protocore::registerVoteInternal', (accounts) => {
       );
     });
   });
+
+  contract('Positive Tests', async () => {
+    it('checks that a vote of a validator is not double-counting', async () => {
+    });
+    it('checks that if a quorum reached link is justified', async () => {
+    });
+    it('checks that if a quorum reached a finalisation link is marked as finalised '
+      + 'and coconsensus is notified', async () => {
+    });
+  });
 });
