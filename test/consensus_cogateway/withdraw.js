468e23faf57e94776d78dbf570e8f32bc164b3cd
Gulshan Vasnani <gulshan@ost.com>
2020-02-03 20:56:16 +0530

    Added withdraw method in ConsensusCogateway; Added UtilityTokenInterface; burn and burnFrom methods in MockToken; Unit test cases;

---
+++ test/consensus_cogateway/withdraw.js
@@ -0,0 +1,124 @@
+// Copyright 2020 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const BN = require('bn.js');
+
+const MockToken = artifacts.require('MockToken');
+const ConsensusCogateway = artifacts.require('ConsensusCogateway');
+const MockConsensus = artifacts.require('MockConsensus');
+
+const { AccountProvider } = require('../test_lib/utils.js');
+const Utils = require('../test_lib/utils.js');
+
+contract('ConsensusCogateway::withdraw', (accounts) => {
+  const accountProvider = new AccountProvider(accounts);
+  let param;
+  let utmost;
+
+  let consensusCogateway;
+  beforeEach(async () => {
+    consensusCogateway = await ConsensusCogateway.new();
+    const owner = accountProvider.get();
+    utmost = await MockToken.new(18, { from: owner });
+    param = {
+      metachainId: Utils.getRandomHash(),
+      utmostAddress: utmost.address,
+      owner,
+      consensusGateway: accountProvider.get(),
+      stateRootProvider: accountProvider.get(),
+      maxStorageRootItems: new BN(100),
+      amount: new BN(100),
+      beneficiary: accountProvider.get(),
+      feeGasPrice: new BN(1),
+      feeGasLimit: new BN(1)
+    };
+
+    const consensusConfig = {
+      metachainId: param.metachainId,
+      epochLength: new BN(100),
+      minValidatorCount: new BN(5),
+      validatorJoinLimit: new BN(20),
+      height: new BN(0),
+      parent: Utils.ZERO_BYTES32,
+      gasTarget: new BN(10),
+      dynasty: new BN(0),
+      accumulatedGas: new BN(1),
+      sourceBlockHeight: new BN(0),
+    };
+
+    const consensus = await MockConsensus.new(
+      consensusConfig.metachainId,
+      consensusConfig.epochLength,
+      consensusConfig.minValidatorCount,
+      consensusConfig.validatorJoinLimit,
+      consensusConfig.height,
+      consensusConfig.parent,
+      consensusConfig.gasTarget,
+      consensusConfig.dynasty,
+      consensusConfig.accumulatedGas,
+      consensusConfig.sourceBlockHeight,
+    );
+
+    await consensusCogateway.setup(
+      param.metachainId,
+      consensus.address,
+      param.utmostAddress,
+      param.consensusGateway,
+      new BN(4),
+      param.maxStorageRootItems,
+      new BN(1)
+    );
+  });
+
+  it('should successfully withdraw', async () => {
+    const beforeUtmostBalanceWithdrawer = await utmost.balanceOf(param.owner);
+
+    await utmost.approve(consensusCogateway.address, param.amount, {
+      from: param.owner,
+    });
+
+    const messageHash = await consensusCogateway.withdraw.call(
+      param.amount,
+      param.beneficiary,
+      param.feeGasPrice,
+      param.feeGasLimit,
+    );
+
+    await consensusCogateway.withdraw(
+      param.amount,
+      param.beneficiary,
+      param.feeGasPrice,
+      param.feeGasLimit,
+    );
+
+    const afterUtmostBalanceWithdrawer = await utmost.balanceOf(param.owner);
+
+    const messageStatus = await consensusCogateway.outbox.call(messageHash);
+
+    assert.isOk(messageStatus === true, 'Message status must be true');
+
+    assert.isOk(
+      afterUtmostBalanceWithdrawer.eq(
+        beforeUtmostBalanceWithdrawer.sub(param.amount),
+      ),
+      'Deposit amount must be transferred from redeemer.'
+        + ` Expected balance is ${beforeUtmostBalanceWithdrawer
+          .sub(param.amount)
+          .toString(10)} but`
+        + `found ${afterUtmostBalanceWithdrawer.toString(10)}`,
+    );
+  });
+});
