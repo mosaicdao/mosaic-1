9b1fcd041af7060015a4da16d8acc80bd6f958f2
bruce9steve <shahrukh@ost.com>
2020-02-05 15:14:41 +0530

    Changed Message bus name in MessageInboxDouble

--- contracts/test/message_bus/MessageInboxDouble.sol
+++ contracts/test/message_bus/MessageInboxDouble.sol
@@ -14,7 +14,7 @@ pragma solidity >=0.5.0 <0.6.0;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import "../../message-Bus/MessageInbox.sol";
+import "../../message-bus/MessageInbox.sol";
 
 /**
  * @title MessageInbox - Contract to confirm the messages declared in MessageOutbox
