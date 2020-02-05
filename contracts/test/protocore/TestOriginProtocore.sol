f3a32b0561d80e125d7defe600f306e84946f939
Deepesh K N <deepesh@ost.com>
2020-02-05 13:15:39 +0530

    Updated unit tests for Coconsensus::finaliseCheckpoint

--- contracts/test/protocore/TestOriginProtocore.sol
+++ contracts/test/protocore/TestOriginProtocore.sol
@@ -71,4 +71,23 @@ contract TestOriginProtocore is OriginProtocore {
     {
         return links[_voteMessageHash].fvsVoteCount[_height];
     }
+
+    /**
+     * @notice This function is used to test the
+     *         `Coconsensus::finaliseCheckpoint`, the msg.sender for the
+     *         `Coconsensus::finaliseCheckpoint` can only be protocore.
+     */
+    function testFinaliseCheckpoint(
+        bytes32 _metachainId,
+        uint256 _blockNumber,
+        bytes32 _blockHash
+    )
+        external
+    {
+        getCoconsensus().finaliseCheckpoint(
+            _metachainId,
+            _blockNumber,
+            _blockHash
+        );
+    }
 }
