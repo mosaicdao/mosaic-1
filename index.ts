00e2cc1ac134325503e0f8cbc356f2057c044b0a
abhayks1 <abhay@ost.com>
2020-02-26 15:52:47 +0530

    export default index.ts modules

--- index.ts
+++ index.ts
@@ -15,4 +15,4 @@
 import * as contracts from './contract_build/contracts.json';
 import interacts from './interacts/Interacts';
 
-export {contracts, interacts};
+export default {contracts, interacts};
